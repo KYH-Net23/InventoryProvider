d6499e754dc93cdbafe8ae22523725fead9f90e5
Isabel Haataja <isabelhaataja@gmail.com>
2024-11-26 14:16:42 +0100

    Installed package Swashbuckle.AspNetCore, added services to container and configured http request pipeline

--- InventorySolution/InventoryProvider/Program.cs
+++ InventorySolution/InventoryProvider/Program.cs
@@ -1,34 +1,45 @@
+using InventoryProvider.Contexts;
+using InventoryProvider.Interfaces;
+using InventoryProvider.Repositories;
+using InventoryProvider.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
+
+builder.Services.AddDbContext<InventoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(o =>
+    o.AddPolicy("AllowAll",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+        .WithMethods("PUT")
+        .AllowAnyHeader();
+    }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
+app.UseHsts();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
