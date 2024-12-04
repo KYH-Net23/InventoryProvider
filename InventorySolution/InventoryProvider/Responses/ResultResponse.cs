a98457c7e25105b12ea935ab4807dd3f52790671
Isabel Haataja <isabelhaataja@gmail.com>
2024-12-04 13:31:17 +0100

    Fixed bug, added json formatted response

--- InventorySolution/InventoryProvider/Responses/ResultResponse.cs
+++ InventorySolution/InventoryProvider/Responses/ResultResponse.cs
@@ -2,7 +2,19 @@
 {
     public static class ResultResponse
     {
-        public static string Success {  get; set; } = "Operation was successful";
+        public static string Success { get; set; } = "Operation was successful";
         public static string Failed { get; set; } = "Operation failed.";
+
+        // Method to return success response in JSON format
+        public static object SuccessResponse()
+        {
+            return new { message = Success, success = true };
+        }
+
+        // Method to return failure response in JSON format
+        public static object FailedResponse()
+        {
+            return new { message = Failed, success = false };
+        }
     }
 }
